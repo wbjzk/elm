f475a01b566501852113637f6f03184b6dcd7403
wbjzk <m18320898462@163.com>
2018-10-28 15:53:46 +0800

    全局样式设置及导航栏下边框设置

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ export default {
 </script>
 
 <style lang="less">
+@import './common/style/mixin.less';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -34,6 +35,7 @@ export default {
   width: 100%;
   height: 40px;
   line-height: 40px;
+  .border-1px(rgba(7,17,27,.1));
   .tab-item {
     flex: 1;
     text-align: center;
