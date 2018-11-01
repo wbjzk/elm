9e8cc236fd52fabebfe447bcc3c7c4fe3b8bcde8
wbjzk <m18320898462@163.com>
2018-11-01 08:23:54 +0800

    设置头部背景效果

--- src/App.vue
+++ src/App.vue
@@ -41,14 +41,14 @@ export default {
 
 <style lang="less">
 @import './common/style/mixin.less';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 .tab {
   display: flex;
   width: 100%;
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -25,6 +25,9 @@
 		<div class="bulletin-wrapper">
 			<span class="bulletin-title"></span><span class="bulletin-text">{{ seller.bulletin }}</span><i class="icon-keyboard_arrow_right"></i>
 		</div>
+		<div class="background">
+			<img :src="seller.avatar" width="100%" height="100%">
+		</div>
 	</div>
 </template>
 
@@ -46,8 +49,10 @@ export default {
 <style lang="less">
 @import "../../common/style/mixin.less";
 .header {
+	position: relative;
 	color: #fff;
-	background: #999;
+	overflow: hidden;
+	background: rgba(7,17,27,.5);
 	.content-wrapper {
 		position: relative;
 		padding: 24px 12px 18px 24px;
@@ -169,6 +174,15 @@ export default {
 			right: 12px;
 			top: 8px;
 		}
+	}
+	.background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		filter: blur(10px);
 	}
 }
 </style>
