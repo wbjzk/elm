cc54f78660bbe3c09ebcacfe5e9e7a7496c053db
wbjzk <m18320898462@163.com>
2018-11-26 09:01:14 +0800

    goods左右布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,7 +1,25 @@
 <template>
-	<div>
-		goods
+	<div class="goods">
+		<div class="menu-wrapper"></div>
+		<div class="foods-wrapper"></div>
 	</div>
 </template>
 <script></script>
-<style></style>
+<style lang="less" scoped>
+.goods {
+	display: flex;
+	position: absolute;
+	top: 174px;
+	bottom: 46px;
+	width: 100%;
+	overflow: hidden;
+	.menu-wrapper {
+		flex: 0 0 80px;
+		width: 80px;
+		background: #f3f5f7;
+	}
+	.foods-wrapper {
+		flex: 1;
+	}
+}
+</style>
