cb779a84e01d997775b4437b2f974fb3bbcb7328
wbjzk <m18320898462@163.com>
2018-11-28 09:36:42 +0800

    设置右侧边栏并渲染样式

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -9,7 +9,32 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper"></div>
+		<div class="foods-wrapper">
+			<ul>
+				<li class="food-list" v-for="(item, i) in goods" :key="i">
+					<h2 class="title">{{ item.name }}</h2>
+					<ul>
+						<li class="food-item" v-for="(food, i) in item.foods" :key="i">
+							<div class="icon">
+								<img :src="food.icon" width="57" height="57">
+							</div>
+							<div class="content">
+								<h3 class="name">{{ food.name }}</h3>
+								<p class="desc">{{ food.description }}</p>
+								<div class="extra">
+									<span class="count">月售{{ food.sellCount }}</span>
+									<span>好评率{{ food.rating }}</span>
+								</div>
+								<div class="price">
+									<span class="now">￥{{ food.price }}</span>
+									<span class="old" v-show="food.oldPrice">{{ food.oldPrice }}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 <script>
@@ -78,6 +103,69 @@ export default {
 	}
 	.foods-wrapper {
 		flex: 1;
+		.food-list {
+			.title {
+				padding-left: 14px;
+				height: 26px;
+				line-height: 26px;
+				border-left: 2px solid #d9dde1;
+				font-size: 12px;
+				color: rgb(147,153,159);
+				background: #f4f5f7;
+			}
+			.food-item {
+				display: flex;
+				margin: 18px;
+				padding-bottom: 18px;
+				.border-1px(rgba(7,17,27,.1));
+				&:last-child {
+					.border-none();
+					margin-bottom: 0;
+				}
+				.icon {
+					flex: 0 0 57px;
+					margin-right: 10px;
+				}
+				.content {
+					flex: 1;
+					.name {
+						margin: 2px 0 8px 0;
+						height: 14px;
+						line-height: 14px;
+						font-size: 14px;
+						color: rgb(7,17,27);
+					}
+					.desc,
+					.extra {
+						line-height: 10px;
+						font-size: 10px;
+						color: rgb(147,153,159);
+					}
+					.desc {
+						margin-bottom: 8px;
+					}
+					.extra {
+						& .count {
+							margin-right: 12px;
+						}
+					}
+					.price {
+						font-weight: bold;
+						line-height: 24px;
+						.now {
+							margin-right: 8px;
+							font-size: 14px;
+							color: rgb(240, 20, 20);
+						}
+						.old {
+							text-decoration: line-through;
+							font-size: 10px;
+							color: rgb(147, 153, 159);
+						}
+					}
+				}
+			}
+		}
 	}
 }
 </style>
