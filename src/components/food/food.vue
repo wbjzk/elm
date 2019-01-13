30d0df6d6b804ba2629b341ee263aa1e62e5c486
wbjzk <m18320898462@163.com>
2019-01-13 19:21:04 +0800

    商品评价

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -51,6 +51,40 @@
                     <p class="text">{{ food.info }}</p>
                 </div>
                 <!-- 商品信息 end -->
+
+                <!-- 分隔间隙 start -->
+                <v-split></v-split>
+                <!-- 分隔间隙 end -->
+
+                <!-- 商品评价 start -->
+                <div class="rating">
+                    <h2 class="title">商品评价</h2>
+                    <v-ratingselect
+                        @select="select"
+                        @toggle="toggleContent"
+                        :select-type="selectType"
+                        :only-content="onlyContent"
+                        :desc="desc"
+                        :ratings="food.ratings">
+                    </v-ratingselect>
+                    <div class="rating-wrapper">
+                        <ul v-show="food.ratings && food.ratings.length">
+                            <li class="rating-item" v-for="rating in food.ratings" :key="rating.username">
+                                <div class="user">
+                                    <span class="name">{{ rating.username }}</span>
+                                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                                </div>
+                                <div class="time">{{ rating.rateTime }}</div>
+                                <p class="text">
+                                    <span :class="{ 'icon-thumb_up': rating.rateType === 0 , 'icon-thumb_down': rating.rateType ===1 }"></span>{{ rating.text }}
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+                    </div>
+                </div>
+                <!-- 商品评价 end -->
+
             </div>
         </div>
     </transition>
@@ -59,6 +93,12 @@
 import BScroll from 'better-scroll';
 import cartcontrol from '../cartcontrol/cartcontrol.vue';
 import split from '../split/split.vue';
+import ratingselect from '../ratingselect/ratingselect.vue';
+
+const POSITIVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
+
 export default {
     props: {
         food: {
@@ -67,13 +107,22 @@ export default {
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            selectType: ALL, // 商品评价：全部-All、满意-NEGATIVE、不满意-POSITIVE
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽',
+            },
         };
     },
     methods: {
         show() {
             this.showFlag = true;
             // 详情页显示的时候设置可以单击且可以滚动
+            this.selectType = ALL;
+            this.onlyContent = true;
             this.$nextTick(() => {
                 if(!this.scroll) {
                     this.scroll = new BScroll(this.$refs.food, {
@@ -96,15 +145,29 @@ export default {
         },
         drop(target) {
             this.$emit('drop', event.target);
+        },
+        select(obj) {
+            if (!obj.event._constructed) {
+                return;
+            }
+            this.selectType = obj.type;
+        },
+        toggleContent(event) {
+            if(!event._constructed) {
+                return;
+            }
+            this.onlyContent = !this.onlyContent;
         }
     },
     components: {
         'v-cartcontrol': cartcontrol,
         'v-split': split,
+        'v-ratingselect': ratingselect,
     }
 };
 </script>
 <style lang="less" scoped>
+@import "../../common/style/mixin.less";
 .food {
     position: fixed;
     left: 0;
@@ -220,6 +283,63 @@ export default {
                 padding: 0 8px;
                 font-size: 12px;
                 color: rgb(77, 85, 93);
+            }
+        }
+        >.rating {
+            padding-top: 18px;
+            >.title {
+                line-height: 14px;
+                margin-left: 18px;
+                font-size: 14px;
+                color: rgb(7, 17, 27);
+            }
+            >.rating-wrapper {
+                padding: 0 18px;
+                .rating-item {
+                    position: relative;
+                    padding: 16px 0;
+                    .border-1px(rgba(7, 17, 27, .1));
+                    >.user {
+                        position: absolute;
+                        right: 0;
+                        top: 16px;
+                        line-height: 12px;
+                        font-size: 0;
+                        >.name {
+                            display: inline-block;
+                            margin-right: 6px;
+                            vertical-align: top;
+                            font-size: 10px;
+                            color: rgb(147, 153, 159);
+                        }
+                        >.avatar {
+                            border-radius: 50%;
+                        }
+                    }
+                    .time {
+                        margin-bottom: 6px;
+                        line-height: 12px;
+                        font-size: 10px;
+                        color: rgb(147, 153, 159);
+                    }
+                    .text {
+                        line-height: 16px;
+                        font-size: 12px;
+                        color: rgb(7, 17, 27);
+                        >.icon-thumb_up,
+                        >.icon-thumb_down {
+                            margin-right: 4px;
+                            line-height: 16px;
+                            font-size: 12px;
+                        }
+                        >.icon-thumb_up {
+                            color: rgb(0, 160, 220);
+                        }
+                        >.icon-thumb_down {
+                            color: rgb(147,153,159);
+                        }
+                    }
+                }
             }
         }
     }
